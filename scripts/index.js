0a5293e300bc62eaddc77cc8939d60ad59112f33
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2019-12-01 16:47:06 -0800

    colorize on hover

--- scripts/index.js
+++ scripts/index.js
@@ -8,8 +8,32 @@ $("#bio-intro").hover(function(){
   $("#jeff-pic").toggleClass('img-color')
 });
 
-// NOVEMBER PROJECT PIC
+// NOVEMBER PROJECT PICS
 $("#november-2019").hover(function(){
   console.log('november-2019 section')
   $(".november-project-pictures").toggleClass('img-color')
+});
+
+// SEPT/OCT PROJECT PICS
+$("#september-october-2019").hover(function(){
+  console.log('september-october-2019 section')
+  $(".sept-oct-project-pictures").toggleClass('img-color')
+});
+
+// BOLD LOGO
+$("#bold").hover(function(){
+  console.log('bold section')
+  $(".bold-logo").toggleClass('img-color')
+});
+
+// Project Management LOGO
+$("#project-management").hover(function(){
+  console.log('project-management section')
+  $(".sfsu-logo").toggleClass('img-color')
+});
+
+// Single Grain LOGO
+$("#single-grain").hover(function(){
+  console.log('single-grain section')
+  $(".single-grain-logo").toggleClass('img-color')
 });
