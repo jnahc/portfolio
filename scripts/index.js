01cc826367a6cdf8bb64df09f6930fdfe1b937e9
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2019-12-01 16:24:46 -0800

    adding in hover colorize

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,15 @@
 // console.log('yup')
 
+// PICTURE COLORIZE SECTION
+
+// SELF PIC
 $("#bio-intro").hover(function(){
-  console.log('bio-intro section')})
+  console.log('bio-intro section')
+  $("#jeff-pic").toggleClass('img-color')
+});
+
+// NOVEMBER PROJECT PIC
+$("#november-2019").hover(function(){
+  console.log('november-2019 section')
+  $(".november-project-pictures").toggleClass('img-color')
+});
